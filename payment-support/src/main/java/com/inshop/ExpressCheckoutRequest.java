36959caa12f983099c1ea48484535f3b17a34d07
Sevak Avetisyan <sevak.avet@gmail.com>
2015-10-10 17:01:21 +0300

    Create payment-support module

---
+++ payment-support/src/main/java/com/inshop/ExpressCheckoutRequest.java
@@ -0,0 +1,125 @@
+package com.inshop;
+
+import com.paypal.core.credential.SignatureCredential;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import urn.ebay.api.PayPalAPI.*;
+import urn.ebay.apis.CoreComponentTypes.BasicAmountType;
+import urn.ebay.apis.eBLBaseComponents.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Created by savetisyan on 09/10/15.
+ */
+public class ExpressCheckoutRequest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExpressCheckoutRequest.class);
+
+    private SignatureCredential signatureCredential;
+    private Properties sdkProperties;
+    private List<PaymentDetailsItemType> items;
+    private SellerDetailsType sellerDetails;
+    private AddressType shipToAddress;
+    private BasicAmountType orderTotal;
+
+    public ExpressCheckoutRequest(List<PaymentDetailsItemType> items, SellerDetailsType sellerDetails,
+                                  AddressType shipToAddress, BasicAmountType orderTotal,
+                                  SignatureCredential signatureCredential, Properties sdkProperties) {
+        this.signatureCredential = signatureCredential;
+        this.sdkProperties = sdkProperties;
+        this.items = items;
+        this.sellerDetails = sellerDetails;
+        this.shipToAddress = shipToAddress;
+        this.orderTotal = orderTotal;
+    }
+
+    public SetExpressCheckoutResponseType setExpressCheckout(String returnUrl, String cancelUrl) {
+        SetExpressCheckoutRequestDetailsType expressCheckoutRequestDetails = new SetExpressCheckoutRequestDetailsType();
+        expressCheckoutRequestDetails.setReturnURL(returnUrl);
+        expressCheckoutRequestDetails.setCancelURL(cancelUrl);
+
+        List<PaymentDetailsType> paymentDetails = new ArrayList<>();
+
+        PaymentDetailsType paymentDetail = new PaymentDetailsType();
+        paymentDetail.setOrderTotal(orderTotal);
+        paymentDetail.setPaymentAction(PaymentActionCodeType.ORDER);
+        paymentDetail.setSellerDetails(sellerDetails);
+        paymentDetail.setPaymentRequestID("request_id");
+
+        paymentDetail.setPaymentDetailsItem(items);
+        paymentDetail.setShipToAddress(shipToAddress);
+        paymentDetails.add(paymentDetail);
+
+        expressCheckoutRequestDetails.setPaymentDetails(paymentDetails);
+
+        SetExpressCheckoutReq setExpressCheckoutReq = new SetExpressCheckoutReq();
+        SetExpressCheckoutRequestType setExpressCheckoutRequest = new SetExpressCheckoutRequestType(expressCheckoutRequestDetails);
+        setExpressCheckoutReq.setSetExpressCheckoutRequest(setExpressCheckoutRequest);
+
+        PayPalAPIInterfaceServiceService service = new PayPalAPIInterfaceServiceService(sdkProperties);
+        SetExpressCheckoutResponseType setExpressCheckoutResponse = null;
+        try {
+            setExpressCheckoutResponse = service.setExpressCheckout(setExpressCheckoutReq, signatureCredential);
+        } catch (Exception e) {
+            LOGGER.warn("Error Message : " + e.getMessage());
+        }
+
+        return setExpressCheckoutResponse;
+    }
+
+    public DoExpressCheckoutPaymentResponseType doExpressCheckout(String token, String payerId) {
+        DoExpressCheckoutPaymentReq doExpressCheckoutPaymentReq = new DoExpressCheckoutPaymentReq();
+
+        DoExpressCheckoutPaymentRequestDetailsType doExpressCheckoutPaymentRequestDetails = new DoExpressCheckoutPaymentRequestDetailsType();
+        doExpressCheckoutPaymentRequestDetails.setToken(token);
+        doExpressCheckoutPaymentRequestDetails.setPayerID(payerId);
+
+        List<PaymentDetailsType> paymentDetails = new ArrayList<>();
+
+        PaymentDetailsType paymentDetail = new PaymentDetailsType();
+        paymentDetail.setOrderTotal(orderTotal);
+        paymentDetail.setPaymentAction(PaymentActionCodeType.ORDER);
+        paymentDetail.setSellerDetails(sellerDetails);
+        paymentDetail.setPaymentRequestID("request_id");
+
+        paymentDetail.setPaymentDetailsItem(items);
+        paymentDetail.setShipToAddress(shipToAddress);
+        paymentDetails.add(paymentDetail);
+
+        doExpressCheckoutPaymentRequestDetails.setPaymentDetails(paymentDetails);
+
+        DoExpressCheckoutPaymentRequestType doExpressCheckoutPaymentRequest = new DoExpressCheckoutPaymentRequestType(doExpressCheckoutPaymentRequestDetails);
+        doExpressCheckoutPaymentReq.setDoExpressCheckoutPaymentRequest(doExpressCheckoutPaymentRequest);
+
+        PayPalAPIInterfaceServiceService service = new PayPalAPIInterfaceServiceService(sdkProperties);
+        DoExpressCheckoutPaymentResponseType doExpressCheckoutPaymentResponse = null;
+        try {
+            doExpressCheckoutPaymentResponse = service.doExpressCheckoutPayment(doExpressCheckoutPaymentReq, signatureCredential);
+        } catch (Exception e) {
+            LOGGER.warn("Error Message : " + e.getMessage());
+        }
+        return doExpressCheckoutPaymentResponse;
+    }
+
+    public GetExpressCheckoutDetailsResponseType getExpressCheckoutDetails(String token) {
+        GetExpressCheckoutDetailsReq getExpressCheckoutDetailsReq = new GetExpressCheckoutDetailsReq();
+        GetExpressCheckoutDetailsRequestType getExpressCheckoutDetailsRequest = new GetExpressCheckoutDetailsRequestType(token);
+
+        getExpressCheckoutDetailsReq.setGetExpressCheckoutDetailsRequest(getExpressCheckoutDetailsRequest);
+        PayPalAPIInterfaceServiceService service = new PayPalAPIInterfaceServiceService(sdkProperties);
+        GetExpressCheckoutDetailsResponseType getExpressCheckoutDetailsResponse = null;
+
+        try {
+            getExpressCheckoutDetailsResponse =
+                    service.getExpressCheckoutDetails(getExpressCheckoutDetailsReq, signatureCredential);
+        } catch (Exception e) {
+            LOGGER.warn("Error Message : " + e.getMessage());
+        }
+
+        return getExpressCheckoutDetailsResponse;
+    }
+
+
+}
---
+++ payment-support/src/main/java/com/inshop/PermissionsRequest.java
@@ -0,0 +1,61 @@
+package com.inshop;
+
+import com.paypal.svcs.services.PermissionsService;
+import com.paypal.svcs.types.perm.GetAccessTokenRequest;
+import com.paypal.svcs.types.perm.GetAccessTokenResponse;
+import com.paypal.svcs.types.perm.RequestPermissionsRequest;
+import com.paypal.svcs.types.perm.RequestPermissionsResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Created by savetisyan on 10/10/15.
+ */
+public class PermissionsRequest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PermissionsRequest.class);
+    private String callback;
+    private Properties sdkProperties;
+
+    public PermissionsRequest(String callback, Properties sdkProperties) {
+        this.callback = callback;
+        this.sdkProperties = sdkProperties;
+    }
+
+    public RequestPermissionsResponse requestPermissions() {
+        List<String> scopeList = Arrays.asList("EXPRESS_CHECKOUT");
+
+        RequestPermissionsRequest requestPermissionsRequest = new RequestPermissionsRequest(scopeList, callback);
+        PermissionsService service = new PermissionsService(sdkProperties);
+        RequestPermissionsResponse requestPermissionsResponse = null;
+
+        try {
+            requestPermissionsResponse = service.requestPermissions(requestPermissionsRequest);
+        } catch (Exception e) {
+            LOGGER.warn("Error Message : " + e.getMessage());
+        }
+
+        return requestPermissionsResponse;
+    }
+
+    public GetAccessTokenResponse getAccessToken(String token, String verifier) {
+        GetAccessTokenRequest getAccessTokenRequest = new GetAccessTokenRequest();
+        getAccessTokenRequest.setToken(token);
+        getAccessTokenRequest.setVerifier(verifier);
+
+        PermissionsService service = new PermissionsService(sdkProperties);
+        GetAccessTokenResponse getAccessTokenResponse = null;
+
+        try {
+            getAccessTokenResponse = service.getAccessToken(getAccessTokenRequest);
+        } catch (Exception e) {
+            LOGGER.warn("Error Message : " + e.getMessage());
+        }
+
+        return getAccessTokenResponse;
+    }
+}
